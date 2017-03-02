d3eaefe69da3c65ed1e78c2a2a0fdb051742fff8
Adrian Statescu <mergesortv@gmail.com>
2017-03-01 18:47:08 -0800

    Ive added some new things

--- stack.c
+++ stack.c
@@ -48,6 +48,43 @@ void create(Stack **s) {
      }
 }
 
+void push(Data data, Stack **stack) {
+
+     Stack *q = (Stack*)malloc(sizeof(Stack));
+
+            q->elem = data;
+
+            q->next = *stack;
+
+            *stack = q;
+}
+
+void pop(Stack **stack) {
+
+     Stack *q = *stack;
+
+            *stack = (*stack)->next;
+
+            free(q); 
+}
+
+Data top(Stack **stack) {
+
+     Data theTop;
+
+     if( isEmpty(*stack) == 0) return;
+
+     theTop = (*stack)->elem;
+
+     Stack *st = *stack;
+
+          (*stack) = (*stack)->next;
+
+     free( st );
+
+     return theTop; 
+}
+
 void display(Stack *stack) {
 
      while( stack ) {
@@ -58,13 +95,51 @@ void display(Stack *stack) {
      }
 }
 
+int isEmpty(Stack *stack) {
+
+    if(stack == NULL) return 0;
+
+         else return 1;
+}
+
 int main() {
 
  Stack *stack;
+ Data ob;
 
+ //create the object abstrack Stack
  create(&stack);
-
+ //display the Stack
  display( stack );
+
+ //add a object into Stack and display it
+ printf("Name=");
+ scanf("%s", ob.name);
+ printf("Code=");
+ scanf("%d", &ob.code);
+ push(ob, &stack);
+ display(stack);
+
+ //pop Stack
+ pop(&stack);
+ printf("Pop()\n");
+ //display after this Pop
+ display(stack);
+ 
+ //get the Top from Stack
+ ob = top(&stack);
+ printf("Top=(%s,%d)\n", ob.name, ob.code);
+
+ display(stack);
+
+ //add a object into Stack and display it
+ printf("Name=");
+ scanf("%s", ob.name);
+ printf("Code=");
+ scanf("%d", &ob.code);
+ push(ob, &stack);
+ display(stack);
+
 
  return 0;
 }
